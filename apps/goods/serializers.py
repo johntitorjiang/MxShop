b6223fa14f4a4cdf11d717ca3368b1279762d32f
LBruse <1219449653@qq.com>
2017-11-02 20:02:45 +0800

    success:serializers.Serializer补充

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -2,10 +2,15 @@
 __author__ = 'Bruse'
 __date__ = '2017-11-02 19:31'
 
+from goods.models import Goods
+
 from rest_framework import serializers
 
 
 class GoodsSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, max_length=100)
     click_num = serializers.IntegerField(default=0)
-    goods_front_image=serializers.ImageField()
+    goods_front_image = serializers.ImageField()
+
+    def create(self, validated_data):
+        return Goods.objects.create(**validated_data)
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+
 from .serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,7 +14,15 @@ class GoodsListView(APIView):
     """
     List all goods
     """
+
     def get(self, request, format=None):
         goods = Goods.objects.all()[:10]
         goods_serializer = GoodsSerializer(goods, many=True)
         return Response(goods_serializer.data)
+
+    def post(self, request, format=None):
+        serializer = GoodsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

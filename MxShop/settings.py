e8b586a128adfe9a20cf5591280fefb613978017
LBruse <1219449653@qq.com>
2017-11-02 19:43:20 +0800

    success:apiview 实现商品列表页

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'trade.apps.TradeConfig',
     'user_operation.apps.UserOperationConfig',
     'crispy_forms',
-    'xadmin'
+    'xadmin',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
---
+++ apps/goods/serializers.py
@@ -0,0 +1,11 @@
+# _*_ coding:utf-8 _*_
+__author__ = 'Bruse'
+__date__ = '2017-11-02 19:31'
+
+from rest_framework import serializers
+
+
+class GoodsSerializer(serializers.Serializer):
+    name = serializers.CharField(required=True, max_length=100)
+    click_num = serializers.IntegerField(default=0)
+    goods_front_image=serializers.ImageField()

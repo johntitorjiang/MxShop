45c59036376fafe74fa76c0da0571917b2188da1
LBruse <1219449653@qq.com>
2017-11-02 18:22:55 +0800

    success:django view 实现商品列表

---
+++ apps/goods/views_base.py
@@ -0,0 +1,28 @@
+# _*_ coding:utf-8 _*_
+__author__ = 'Bruse'
+__date__ = '2017-11-02 18:09'
+
+from django.views.generic.base import View
+
+from goods.models import Goods
+
+
+class GoodsListView(View):
+    def get(self, request):
+        """
+        通过Django的view实现商品列表页
+        :param request:
+        :return:
+        """
+        json_list = []
+        goods = Goods.objects.all()[:10]
+        for good in goods:
+            json_dict = {}
+            json_dict["name"] = good.name
+            json_dict["category"] = good.category.name
+            json_dict["market_price"] = good.market_price
+            json_list.append(json_dict)
+
+        from django.http import HttpResponse
+        import json
+        return HttpResponse(json.dumps(json_list), content_type="application/json")

cd3e2df474c9b1223534169ba7225dfd2b22f814
LBruse <1219449653@qq.com>
2017-11-03 00:57:35 +0800

    success:drf的搜索和排序

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -7,6 +7,7 @@ from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Goods
@@ -24,10 +25,12 @@ class GoodsPagination(PageNumberPagination):
 
 class GoodsListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
-    商品列表页
+    商品列表页,分页,搜索,过滤，排序
     """
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
     pagination_class = GoodsPagination
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     filter_class = GoodsFilter
+    search_fields = ('name', 'goods_brief', 'goods_desc')
+    ordering_fields = ('sold_num', 'add_time')

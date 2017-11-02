8f2ebee01b728f4cce888aabd789b379a98ce79a
LBruse <1219449653@qq.com>
2017-11-02 20:15:47 +0800

    success:modelserializer实现商品列表页功能

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -19,10 +19,3 @@ class GoodsListView(APIView):
         goods = Goods.objects.all()[:10]
         goods_serializer = GoodsSerializer(goods, many=True)
         return Response(goods_serializer.data)
-
-    def post(self, request, format=None):
-        serializer = GoodsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
